a3f5699d30f2bd90896a8acab0d6010eee6e87a5
Alexei Chetroi <alexei.chetroi@outlook.com>
2019-11-13 23:00:41 -0500

    Update status tests.

--- tests/test_types_named.py
+++ tests/test_types_named.py
@@ -10,3 +10,24 @@ def test_fake_enum():
     r = fake_enum("test_member", mock.sentinel.value)
     assert r == mock.sentinel.value
     assert mock.sentinel.wrong != r
+
+
+def test_status():
+    """Test Status enum."""
+
+    assert t.Status.Success == 0
+    assert t.Status.Failure == 1
+
+    extra = b"the rest of the owl\xaa\x55\x00\xff"
+
+    r, rest = t.Status.deserialize(b"\x00" + extra)
+    assert rest == extra
+    assert r == 0
+    assert r == t.Status.Success
+    assert r.name == "Success"
+
+    r, rest = t.Status.deserialize(b"\xff" + extra)
+    assert rest == extra
+    assert r == 0xFF
+    assert r.value == 0xFF
+    assert r.name.startswith("unknown")
