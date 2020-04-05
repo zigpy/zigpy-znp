b72c552f923635de3d9672224d0c6d845b092c14
puddly <32534428+puddly@users.noreply.github.com>
2020-04-05 13:30:35 -0400

    Bump patch version

--- zigpy_znp/__init__.py
+++ zigpy_znp/__init__.py
@@ -1,5 +1,6 @@
 MAJOR_VERSION = 0
 MINOR_VERSION = 0
-PATCH_VERSION = "1.dev0"
-__short_version__ = "{}.{}".format(MAJOR_VERSION, MINOR_VERSION)
-__version__ = "{}.{}".format(__short_version__, PATCH_VERSION)
+PATCH_VERSION = "1.dev1"
+
+__short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
+__version__ = f"{__short_version__}.{PATCH_VERSION}"
