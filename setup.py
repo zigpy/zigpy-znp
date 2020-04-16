f60c54cf54b18e40d22984369e9c2a3c4167ce58
puddly <32534428+puddly@users.noreply.github.com>
2020-04-15 23:33:18 -0400

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 """Setup module for zigpy-znp"""
 
+import pathlib
 from setuptools import find_packages, setup
 
 import zigpy_znp
+
+root = pathlib.Path(__file__).parent
 
 setup(
     name="zigpy-znp",
     version=zigpy_znp.__version__,
     description="A library which communicates with ZNP radios for zigpy",
+    long_description=(root / "README.md").read_text(),
+    long_description_content_type="text/markdown",
     url="http://github.com/zha-ng/zigpy-znp",
     author="Alexei Chetroi",
     author_email="alexei.chetroi@outlook.com",
