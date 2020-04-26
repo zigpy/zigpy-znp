2a0698a11faa7ac668893d1252265d8e37b52178
puddly <32534428+puddly@users.noreply.github.com>
2020-04-26 16:24:40 -0400

    Bump patch version

--- zigpy_znp/__init__.py
+++ zigpy_znp/__init__.py
@@ -8,7 +8,7 @@ if not hasattr(logging.getLoggerClass(), "trace"):
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 0
-PATCH_VERSION = 2
+PATCH_VERSION = 3
 
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
