b4a160917a08c5e962f904c771754e139644b842
puddly <32534428+puddly@users.noreply.github.com>
2020-05-01 22:03:49 -0400

    Bring version back down to 0.0.3, since it was never released

--- zigpy_znp/__init__.py
+++ zigpy_znp/__init__.py
@@ -8,7 +8,7 @@ if not hasattr(logging.getLoggerClass(), "trace"):
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 0
-PATCH_VERSION = 4
+PATCH_VERSION = 3
 
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
