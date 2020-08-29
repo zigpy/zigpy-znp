3c9e3301a06d577f010cdade6a99c5b27b6ae5d8
puddly <32534428+puddly@users.noreply.github.com>
2020-08-29 18:45:47 -0400

    Move testing dependencies into `extras_require["testing"]`

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 """Setup module for zigpy-znp"""
 
 import pathlib
-from setuptools import find_packages, setup
-
 import zigpy_znp
 
-root = pathlib.Path(__file__).parent
+from setuptools import find_packages, setup
 
 setup(
     name="zigpy-znp",
     version=zigpy_znp.__version__,
     description="A library for zigpy which communicates with TI ZNP radios",
-    long_description=(root / "README.md").read_text(),
+    long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
     long_description_content_type="text/markdown",
     url="https://github.com/zha-ng/zigpy-znp",
     author="Alexei Chetroi",
@@ -25,10 +23,12 @@ setup(
         "voluptuous",
         "coloredlogs",
     ],
-    tests_require=[
-        "asynctest",
-        "pytest>=5.4.5",
-        "pytest-asyncio>=0.12.0",
-        "pytest-mock",
-    ],
+    extras_require={
+        "testing": [
+            "asynctest",
+            "pytest>=5.4.5",
+            "pytest-asyncio>=0.12.0",
+            "pytest-mock",
+        ]
+    },
 )
