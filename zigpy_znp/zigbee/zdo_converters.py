zigpy/zigpy-znp

commit 469f5900e1b58191233d76ad317a750fb6dbaa3c

ZDO lqi & rtg schema added (#41)
* Add ZDO Mgmt Lqi & Rst schemas

zigpy_znp/zigbee/zdo_converters.py
@@ -116,4 +116,33 @@ ZDO_CONVERTERS = {
         (lambda addr: c.ZDO.BindRsp.Callback(partial=True, Src=addr)),
         (lambda rsp: (ZDOCmd.Bind_rsp, {"Status": rsp.Status})),
     ),
+    ZDOCmd.Mgmt_Lqi_req: (
+        (
+            lambda addr, device, StartIndex: (
+                c.ZDO.MgmtLqiReq.Req(
+                    Dst=addr,
+                    StartIndex=StartIndex,
+                )
+            )
+        ),
+        (lambda addr: c.ZDO.MgmtLqiRsp.Callback(partial=True, Src=addr)),
+        (
+            lambda rsp: (
+                ZDOCmd.Mgmt_Lqi_rsp,
+                {"Status": rsp.Status, "Neighbors": rsp.Neighbors},
+            )
+        ),
+    ),
+    ZDOCmd.Mgmt_Rtg_req: (
+        (
+            lambda addr, StartIndex: (
+                c.ZDO.MgmtLqiReq.Req(
+                    Dst=addr,
+                    StartIndex=StartIndex,
+                )
+            )
+        ),
+        (lambda addr: c.ZDO.MgmtRtgRsp.Callback(partial=True, Src=addr)),
+        (lambda rsp: (ZDOCmd.Mgmt_Rtg_rsp, {"Status": rsp.Status})),
+    ),
 }

zigpy_znp/commands/zdo.py
@@ -1109,7 +1109,7 @@ class ZDO(t.CommandsBase, subsystem=t.Subsystem.ZDO):
             t.Param(
                 "Status", t.ZDOStatus, "Status is either Success (0) or Failure (1)"
             ),
-            t.Param("Neighbours", zigpy.zdo.types.Neighbors, "Neighbours"),
+            t.Param("Neighbors", zigpy.zdo.types.Neighbors, "Neighbors"),
         ),
     )
 

commit a2134d6862ba4442b5e9a5ab2390618f7a96d414

Fix `ZDOCmd.Mgmt_Rtg_req` translation typo introduced in #41

zigpy_znp/zigbee/zdo_converters.py
@@ -137,7 +137,7 @@ ZDO_CONVERTERS = {
     ZDOCmd.Mgmt_Rtg_req: (
         (
             lambda addr, StartIndex: (
-                c.ZDO.MgmtLqiReq.Req(
+                c.ZDO.MgmtRtgReq.Req(
                     Dst=addr.address,
                     StartIndex=StartIndex,
                 )