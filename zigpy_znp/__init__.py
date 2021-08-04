466ceb6d09a35a7e8763efa9d63919e1a21578b4
puddly <32534428+puddly@users.noreply.github.com>
2021-08-04 15:12:18 -0400

    Bump version to 0.5.3

--- zigpy_znp/__init__.py
+++ zigpy_znp/__init__.py
@@ -1,6 +1,6 @@
 MAJOR_VERSION = 0
 MINOR_VERSION = 5
-PATCH_VERSION = 2
+PATCH_VERSION = 3
 
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
