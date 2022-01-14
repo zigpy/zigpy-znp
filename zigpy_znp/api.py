9ba02f1ec78713cd34743dc320ae3da31f367339
puddly <32534428+puddly@users.noreply.github.com>
2022-01-14 12:58:02 -0500

    Reduce unnecessary logging verbosity
    Unhandled commands will be logged completely in the preceding line

--- zigpy_znp/api.py
+++ zigpy_znp/api.py
@@ -718,7 +718,7 @@ class ZNP:
         Called when a command that is not handled by any listener is received.
         """
 
-        LOGGER.debug("Command was not handled: %s", command)
+        LOGGER.debug("Command was not handled")
 
     @contextlib.asynccontextmanager
     async def capture_responses(self, responses):
