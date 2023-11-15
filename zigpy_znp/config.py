19e4fc2754d3f5ccc4ffb1913f106a4e4a7795a5
puddly <32534428+puddly@users.noreply.github.com>
2023-11-15 17:34:44 -0500

    Use zigpy `device` config schema

--- zigpy_znp/config.py
+++ zigpy_znp/config.py
@@ -7,7 +7,6 @@ from zigpy.config import (  # noqa: F401
     CONF_DEVICE,
     CONF_NWK_KEY,
     CONFIG_SCHEMA,
-    SCHEMA_DEVICE,
     CONF_NWK_PAN_ID,
     CONF_DEVICE_PATH,
     CONF_NWK_CHANNEL,
@@ -25,18 +24,6 @@ from zigpy_znp.commands.util import LEDMode
 ConfigType = typing.Dict[str, typing.Any]
 
 VolPositiveNumber = vol.All(numbers.Real, vol.Range(min=0))
-
-CONF_DEVICE_BAUDRATE = "baudrate"
-CONF_DEVICE_FLOW_CONTROL = "flow_control"
-
-SCHEMA_DEVICE = SCHEMA_DEVICE.extend(
-    {
-        vol.Optional(CONF_DEVICE_BAUDRATE, default=115_200): int,
-        vol.Optional(CONF_DEVICE_FLOW_CONTROL, default=None): vol.In(
-            ("hardware", "software", None)
-        ),
-    }
-)
 
 
 def EnumValue(enum, transformer=str):
@@ -101,7 +88,6 @@ CONF_CONNECT_DTR_STATES = "connect_dtr_pin_states"
 
 CONFIG_SCHEMA = CONFIG_SCHEMA.extend(
     {
-        vol.Required(CONF_DEVICE): SCHEMA_DEVICE,
         vol.Optional(CONF_ZNP_CONFIG, default={}): vol.Schema(
             vol.All(
                 {
